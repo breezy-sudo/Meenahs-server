51a07391b4e7a57fc2e043cc359b9286ae0de12d
Muhamad Bilkisu <usmanmbilkisu@gmail.com>
2026-07-07 15:18:26 +0100

    errors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
 const hpp = require('hpp');
 require('dotenv').config();
 
@@ -22,19 +23,20 @@ const allowedOrigins = [
 app.set('trust proxy', 1);
 
 // security middleware
-app.use(helmet());
-app.use(hpp());
+app.use(helmet()); //secure HTTP headers
+app.use(hpp()); // prevent HTTPparameters pollution
 
 // rate limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.'
 });
 app.use(limiter);
 
 // body parser and data sanitization
 app.use(express.json());
+app.use(mongoSanitize()); // sanitize data to prevent NoSQL injection and also remove $ and . from user unput
 app.use(cors({
     origin: (origin, callback) => {
         if (!origin || allowedOrigins.includes(origin)) {
@@ -49,9 +51,9 @@ app.use(cors({
 }));
 
 // connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/meenahshennaart')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Meenahshennaart')
     .then(() => console.log('✅MongoDB connected successfully!'))
-    .catch((err) => console.error('❌MongoDB connection error:', err));
+    .catch(() => console.log('❌MongoDB connection error:', err));
 
 // routes
 const bookingRoutes = require('./routes/instrn.js');
@@ -59,7 +61,12 @@ app.use('/bookings', bookingRoutes);
 app.get('/', (req, res) => {
     res.send('🌸 Meenahs Henna Art Server is running!');
 });
+app.listen(7000, () => {
+    console.log('🚀Server is running on http://localhost:7000');
+})
 
+// start server
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
     console.log(`🚀Server is running on http://localhost:${PORT}`);
-});
+})
