f5dba51e5ae7bc396baf06a5bc79754a67bddcae
Muhamad Bilkisu <usmanmbilkisu@gmail.com>
2026-07-07 13:25:07 +0100

    ANother change

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.use(limiter);
 app.use(express.json());
 app.use(mongoSanitize()); // sanitize data to prevent NoSQL injection and also remove $ and . from user unput
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:5500', 'http://127.0.0.1:5500', 'http://127.0.0.1:5500', 'http://localhost:7000', 'https://meenahs-henna-art.vercel.app'],
+    origin: ['https://vercel.app'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true
 }));
 
